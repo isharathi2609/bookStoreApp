62835b6741333942a0e709c646e5da070e9cb158
Isha Rathi <rathiisha77@gmail.com>
2024-05-29 11:58:14 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import list from "../../public/list.json";
+import Cards from "../components/Cards";
+
+function Freebook() {
+  const filterData = list.filter((data) => data.category === "Free");
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl  container mx-auto md:px-20 px-4">
+        <div>
+        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+        <p>Unlock your potential with our free courses, designed to help you enhance your
+        skills and knowledge. Start learning today at no cost! Join thousands of 
+        learners on their journey to success and gain valuable expertise in various fields</p>
+        </div>
+        
+        
+        
+      
+      <div>
+      <Slider {...settings}>
+        {filterData.map((item)=>{
+           return <Cards item ={item} key ={item.id}/>
+        })}
+      </Slider>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
